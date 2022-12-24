fecd9ea9a6e47d1fc258db8fd8eed275068249e6
VadKempn <90304716+VadKempn@users.noreply.github.com>
2022-12-24 22:07:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,67 @@
 #include <stdio.h>
 
+#define MAX_SIZE 100
+
 int main() {
-    int arr[6]={34, 45, 56, 67, 78, 89};
-    printf("==============================\n");
-    for (int i = 0; i < 6; ++i) {
-        printf("%d | ", arr[i]);
+    int rows1, cols1, rows2, cols2;
+    int matrix1[MAX_SIZE][MAX_SIZE];
+    int matrix2[MAX_SIZE][MAX_SIZE];
+    int result[MAX_SIZE][MAX_SIZE];
+
+    // Read in the dimensions of the matrices
+    printf("Enter the number of rows in matrix 1: ");
+    scanf("%d", &rows1);
+    printf("Enter the number of columns in matrix 1: ");
+    scanf("%d", &cols1);
+    printf("Enter the number of rows in matrix 2: ");
+    scanf("%d", &rows2);
+    printf("Enter the number of columns in matrix 2: ");
+    scanf("%d", &cols2);
+
+    // Check if the matrices can be multiplied
+    if (cols1 != rows2) {
+        printf("Error: the matrices cannot be multiplied\n");
+        return 1;
     }
-    printf("\n==============================");
-    int arr_1[2][2]={{2,1},{1,3}};
-    int arr_2[2][2]={{1,2},{3,1}};
-    int arr_3[2][2]={{0,0},{0,0}};
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            for (int k = 0; k < 2; ++k) {
-                arr_3[i][j] += arr_1[i][k] * arr_2[k][j];
+
+    // Read in the values of the matrices
+    printf("Enter the values of matrix 1:\n");
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+    printf("Enter the values of matrix 2:\n");
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    // Initialize the result matrix to all zeros
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            result[i][j] = 0;
+        }
+    }
+
+    // Perform matrix multiplication
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            for (int k = 0; k < cols1; k++) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
             }
         }
     }
-    printf("\n%d %d\n%d %d", arr_3[0][0], arr_3[0][1], arr_3[1][0], arr_3[1][1]);
+
+    // Print the result matrix
+    printf("The result is:\n");
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
